4f3d20b5738948db71b8457e7558c89d5e9d9e80
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-10 22:04:02 +0100

    front-end-pro

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
 import React from "react"
-import fond from "./assets/fondecran.png"
-import { useSpring, animated } from 'react-spring';
 import "./App.css"
-import Trpc from "./anim"
+import Home from "./components/home"
 export default function App() {
   return (
     <>
-<header>
-    <div className="container">
-    <div className="logo">
-      <h2>MyDesign</h2>
-    </div>
-      <div className="nav">    
-      <div><h3>Home</h3></div>
-      <div><h3>Service</h3></div>
-      <div> <h3>Contact</h3></div>
-      </div>
-    </div>
-    {/*Deuxiemme partie */}
-    <div className="Home">
-    <div className="prensentation">
-    <h1><Trpc/></h1>
-    </div>
-    <div className="monimg">
-    <figure className="hero-banner">
-        <img
-          src={fond}
-          width="400px" 
-          height="350px"
-          loading="lazy"
-          alt="hero-banner"
-          className="w-100 banner-animation"
-        />
-      </figure>
-    </div>
-    </div>
-    </header> 
-       </>
+  <Home/>
+    </>
   )
 }
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,29 +1,41 @@
+import React from "react"
+import fond from "../../assets/fondecran.png"
+import { useSpring, animated } from 'react-spring';
 import style from "./index.module.css"
-import React from 'react'
-import fonecran from "../../assets/fondecran.png"
+import Trpc from "../../anim"
 export default function Home() {
   return (
     <>
-    <header>
-    <div className={style.cont}>
+<header>
+    <div className={style.container}>
     <div className={style.logo}>
-      <h1>MyDesign</h1>
+    <h2>MyDesign</h2>
     </div>
-      <div className={style.nav}>      
+      <div className={style.nav}>     
       <div><h3>Home</h3></div>
       <div><h3>Service</h3></div>
       <div> <h3>Contact</h3></div>
       </div>
     </div>
-    <div className={style.contenu}>
-    <div className={style.prensentation}>
-    <h1>We Design Interfaces That Users Love</h1>
+    {/*Deuxiemme partie */}
+    <div className={style.Home}>
+<div className={style.prensentation}>
+    <h1><Trpc/></h1>
     </div>
-    <div className={style.monimg}>
-    <img width="200px" height="200px" src={fonecran}></img>
+    <div className={style.monimg}>  
+    <figure className="hero-banner">
+        <img
+          src={fond}
+          width="500px" 
+          height="400px"
+          loading="lazy"
+          alt="hero-banner"
+          className="w-100 banner-animation"
+        />
+      </figure>
     </div>
     </div>
-    </header>
-  </>
+    </header> 
+       </>
   )
 }
